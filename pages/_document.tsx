04b1af515df814d3dc5b8b8803a0e49d79144444
saeid4x <saeid.almahdi@gmail.com>
2023-10-14 15:39:17 +0330

    first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
+//***Increase timeout for google provider ****/
+import { custom } from 'openid-client'; 
+custom.setHttpOptionsDefaults({
+  timeout: 16000,
+});
+//****
 export default function Document() {
+
+  
   return (
     <Html lang="en">
       <Head />
